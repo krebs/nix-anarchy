3c2a74420048baaf57fbf26de14bd0d2c0c2b3a2
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-10-23 23:32:08 +0200

    shell.nix: add all-ages font

--- shell.nix
+++ shell.nix
@@ -5,8 +5,13 @@ let
     sha256 = "1908wxqgi1jiwdc7kvq8xnb1z31337gca1qpdz353xgv5ndsi7py";
     stripRoot = false;
   };
+  all-ages-font = pkgs.fetchzip {
+    url = "https://www.fontspace.com/download/12184/37e4ed90d3744528bf13e606f1a5e125/kc-fonts_all-ages.zip";
+    sha256 = "0pbs3s1n18qjhjcqwfxc9y95yqgm6n3mpyd5ss0ciil44klg256h";
+    stripRoot = false;
+  };
 in
 pkgs.mkShell {
   buildInputs = [ pkgs.inkscape ];
-  FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [ misdemeanor-font ]; };
+  FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [ all-ages-font misdemeanor-font ]; };
 }
